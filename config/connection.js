85d7f012868c9973d1ac26e1517ae106444c89de
Green Man <brendan.mccaughey@gmail.com>
2018-12-22 14:44:50 -0500

    Set up server and database connection.

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,14 @@
+require('dotenv').config()
+
 // Set up MySQL connection.
 var mysql = require("mysql");
+
 
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "bm8163125",
+  password: process.env.db_PW,
   database: "burgers_db"
 });
 
