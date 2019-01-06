a4d57b506431cb46ad7d6f994b7a3be761150389
Green Man <brendan.mccaughey@gmail.com>
2019-01-06 11:16:24 -0500

    Some positioning and burger image added.

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
 var express = require("express");
 var methodOverride = require('method-override');
+var path = require('path');
 
 var PORT = process.env.PORT || 8080;
 
 var app = express();
 
-// Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
-
 // Parse application body
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Allow the use of PUT and POST directly in the HTML
 app.use(methodOverride('_method'));
 
 // Set Handlebars.
@@ -19,6 +18,10 @@ var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
+// Serve static content for the app from the "public" directory in the application directory.
+// app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, '/public')));
 
 // Import routes and give the server access to them.
 var routes = require("./controllers/burgers_controller.js");
